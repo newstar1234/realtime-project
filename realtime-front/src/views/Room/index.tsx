633d91392c87d5039ea35c06ee9fba42112a7870
newstar1234 <maori36@naver.com>
2023-08-28 18:18:47 +0900

    23.08.28.17:30

--- realtime-front/src/views/Room/index.tsx
+++ realtime-front/src/views/Room/index.tsx
@@ -12,6 +12,8 @@ export default function Room() {
 
 // description : Send Button Ref 상태 //
 const sendButtonRef = useRef<HTMLDivElement | null>(null);
+// description : Send Button Ref 상태 //
+const roomContainerRef = useRef<HTMLDivElement | null>(null);
 
   // description : path 상태 변경 함수 //
   const { setPath } = usePathStore();
@@ -48,6 +50,7 @@ const sendButtonRef = useRef<HTMLDivElement | null>(null);
 
   // description : 전송 버튼 클릭 처리 //
   const onSendButtonClickHandler = () => {
+    if(!message.trim()) return;
     const datetime = moment().format('YYYY-MM-DD hh:mm:ss a');
     const data: MessageDto = { id, room, nickname, message, datetime };
     
@@ -63,7 +66,34 @@ const sendButtonRef = useRef<HTMLDivElement | null>(null);
   }
   socket.on('receive', onReceiveHandler);
   
+//              component : 채팅 메세지 아이템 컴포넌트             //
+interface ChatMessageItemProps {
+  messageItem : MessageDto;
+}
+const ChatMessageItem = ({ messageItem } : ChatMessageItemProps) => {
+  const { nickname, message, datetime } = messageItem;
   
+  if(id === messageItem.id) 
+    return(
+    <div className='message-box-mine'>
+      <div className='message-mine'>{ message }</div>
+      <div className='message-datetime'>{ datetime }</div>
+    </div>
+    );
+    
+    return(
+    <div className='message-box-others'>
+      <div className='message-box-wrapper'>
+        <div className='message-nickname'>{ nickname }</div>
+        <div className='message-container-others'>
+          <div className='message-other'>{ message }</div>
+          <div className='message-datetime'>{ datetime }</div>
+        </div>
+      </div>
+    </div>
+    );
+}
+
   //              effect              //
   // description : 첫 마운트 시 소켓 연결 //
   let effectFlag = true;
@@ -86,6 +116,13 @@ const sendButtonRef = useRef<HTMLDivElement | null>(null);
       socket.emit('join', room);
     }, []);
     
+  // description : 스크롤 자동 이동 //
+    useEffect(() => {
+      if(!roomContainerRef.current) return;
+      roomContainerRef.current.scrollTop = roomContainerRef.current.scrollHeight;
+    }, [messageList]);
+
+
 //              render : 채팅방 컴포넌트 랜더링             //
   return (
     <div id='room'>
@@ -93,8 +130,8 @@ const sendButtonRef = useRef<HTMLDivElement | null>(null);
         <div className='room-number'>{room}</div>
         <div className='room-back-button' onClick={onBackButtonClickHandler} >뒤로</div>
       </div>
-      <div className='room-container'>
-        { messageList.map(messageItem => <div>{`${messageItem.nickname} ${messageItem.message}`}</div>) }
+      <div ref={roomContainerRef} className='room-container'>
+        { messageList.map(messageItem => <ChatMessageItem messageItem={messageItem} />) }
       </div>
       <div className='room-footer'>
         <input className='room-send-input' type='text' value={message} onChange={onMessageChangeHandler} onKeyDown={onEnterKeyDownHandler} /> 
